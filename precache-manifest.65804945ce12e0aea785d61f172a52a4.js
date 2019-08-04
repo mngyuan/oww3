e97640bc1936d84a32d26c6ac968eadedbebd6b2
Kevin Lee <kevinmlee.x@gmail.com>
2019-08-03 21:39:54 -0700

    Updates

---
+++ precache-manifest.65804945ce12e0aea785d61f172a52a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82d771165fff57645dfab87d3d5bab95",
+    "url": "/oww3/index.html"
+  },
+  {
+    "revision": "fdd759e60d23adb8b24a",
+    "url": "/oww3/static/css/main.73d9b388.chunk.css"
+  },
+  {
+    "revision": "a206073f1932634913f5",
+    "url": "/oww3/static/js/2.1dcfa618.chunk.js"
+  },
+  {
+    "revision": "fdd759e60d23adb8b24a",
+    "url": "/oww3/static/js/main.aa9014ff.chunk.js"
+  },
+  {
+    "revision": "e1afb70099510e756d34",
+    "url": "/oww3/static/js/runtime~main.5fcedf43.js"
+  },
+  {
+    "revision": "001a477bbcd2f734c491833f2191c95a",
+    "url": "/oww3/static/media/Apercu Bold.001a477b.woff"
+  },
+  {
+    "revision": "7f08dee7b13ac0050b5231c74d937ccd",
+    "url": "/oww3/static/media/Apercu_Regular.7f08dee7.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oww2/precache-manifest.a448accb52703aeb2f71c3cbe0f246d2.js"
+  "/oww3/precache-manifest.65804945ce12e0aea785d61f172a52a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oww2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/oww3/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
